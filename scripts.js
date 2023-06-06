6dbf14748bdd7f74e0cf52a24243930e6ead0184
simonmysun <simonmysun@gmail.com>
2023-06-06 20:21:35 +0200

    add missing js

--- scripts.js
+++ scripts.js
@@ -34,7 +34,7 @@ if (window.SharedWorker) {
     };
     renderer.port.postMessage(mainDom.innerHTML.replace(/&gt;+/g, '>'));
 } else {
-    (function (url, callback) {
+    var loadJs = function (url, callback) {
         var script = document.createElement('script')
         script.type = 'text/javascript';
         if (script.readyState) {  //IE
@@ -52,7 +52,10 @@ if (window.SharedWorker) {
         }
         script.src = url;
         document.getElementsByTagName("head")[0].appendChild(script);
-    })('/assets/marked.min.js', function () {
-        decorate(mainDom, marked(mainDom.innerHTML.replace(/&gt;+/g, '>')));
+    };
+    loadJs('/assets/marked.min.js', function () {
+        loadJs('/assets/highlight.min.js', function() {
+            decorate(mainDom, hljs.highlightAuto(marked(mainDom.innerHTML.replace(/&gt;+/g, '>'))));
+        }  
     });
 }
