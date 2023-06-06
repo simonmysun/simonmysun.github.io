e37a07b91d69a95df4cdb9ff360cd7a9ec8a8324
simonmysun <simonmysun@gmail.com>
2023-06-06 20:17:49 +0200

    adding highlighejs

--- scripts.js
+++ scripts.js
@@ -4,6 +4,12 @@ remarkdownCss.type = 'text/css';
 remarkdownCss.rel = "stylesheet"
 document.head.append(remarkdownCss);
 
+var hljsCss = document.createElement('link');
+hljsCss.href = '/asstes/hljs-grayscale.css';
+hljsCss.type = 'text/css';
+hljsCss.rel = 'stylesheet'
+document.head.append(hljsCss);
+
 var customCss = document.createElement('link');
 customCss.href = '/styles.css';
 customCss.type = 'text/css';
@@ -12,7 +18,7 @@ document.head.append(customCss);
 
 var mainDom = document.getElementById('main');
 
-var decorate = function(oldDom, newHTML) {
+var decorate = function (oldDom, newHTML) {
     oldDom.innerHTML = newHTML;
     oldDom.className = 'remarkdown h1-underline ul-star em-star strong-star a-showurl code-tick pre-tick';
     oldDom.style.whiteSpace = '';
@@ -21,32 +27,32 @@ var decorate = function(oldDom, newHTML) {
     oldDom.style.margin = '';
 };
 
-if(window.SharedWorker) {
+if (window.SharedWorker) {
     var renderer = new SharedWorker('/worker.js');
-    renderer.port.onmessage = function(e) {
-        decorate(mainDom, e.data)
+    renderer.port.onmessage = function (e) {
+        decorate(mainDom, e.data);
     };
     renderer.port.postMessage(mainDom.innerHTML.replace(/&gt;+/g, '>'));
 } else {
-    (function(url, callback){
+    (function (url, callback) {
         var script = document.createElement('script')
         script.type = 'text/javascript';
-        if (script.readyState){  //IE
-            script.onreadystatechange = function(){
+        if (script.readyState) {  //IE
+            script.onreadystatechange = function () {
                 if (script.readyState == 'loaded' ||
-                    script.readyState == 'complete'){
+                    script.readyState == 'complete') {
                     script.onreadystatechange = null;
                     callback();
                 }
             };
         } else {  //Others
-            script.onload = function(){
+            script.onload = function () {
                 callback();
             };
         }
         script.src = url;
         document.getElementsByTagName("head")[0].appendChild(script);
-    })('/assets/marked.min.js', function() {
+    })('/assets/marked.min.js', function () {
         decorate(mainDom, marked(mainDom.innerHTML.replace(/&gt;+/g, '>')));
     });
 }
--- worker.js
+++ worker.js
@@ -1,9 +1,10 @@
 importScripts('/assets/marked.min.js');
+importScripts('/assets/highlight.min.js');
 
 onconnect = function(e) {
   var port = e.ports[0];
   port.onmessage = function(e) {
-    port.postMessage(marked(e.data));
+    port.postMessage(hljs.highlightAuto(marked(e.data)));
   };
 };
 
